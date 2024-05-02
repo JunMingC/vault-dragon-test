5732cd3abccc4f6d226b9181165dd736826bd988
Jun <jm96.chan@gmail.com>
2024-05-02 13:44:43 +0800

    Implement rate limit
    Uses express-rate-limit package to limit 60 requests per 2 minutes per IP

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ import dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import rateLimit from 'express-rate-limit'
 import { objectRoute } from './routes/object.js'
 
 dotenv.config()
 const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT = process.env.PORT || 5000
+const limiter = rateLimit({
+    windowMs: 2 * 60 * 1000, // 2 minutes
+    max: 60, // limit each IP to 60 requests per windowMs
+    message: 'Error: Rate Limited, too many requests from this IP, please try again in 2 minutes',
+})
 
 const app = express()
 app.use(express.json({ limit: "30mb" }))
 app.use(cors())
+app.use(limiter)
 
 mongoose.connect(CONNECTION_URL)
     .then(() => {
@@ -50,7 +57,7 @@ app.get("/", (_req, res) => {
     `
 
     res.send(`<pre>${response}</pre>`)
-});
+})
 
 app.use((err, _req, res, _next) => {
     console.error(err.stack)
